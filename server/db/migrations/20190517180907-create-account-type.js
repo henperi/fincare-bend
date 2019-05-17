47e09a23bbcf2ecb4d1d1c92b3fca992e71003eb
Henry <pienry3g@gmail.com>
2019-05-17 19:52:55 +0100

    feat(superAdmin): create account types
    - update SuperAdminController: add createAccountTypes
    - create model/migration files for account type
    - update validationMiddleware: add validateCreateAccountType
    - update routes/superAdmin: add createAccountType POST route
    
    [Finishes #166077830]

---
+++ server/db/migrations/20190517180907-create-account-type.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('AccountTypes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    interestRate: {
+      type: Sequelize.INTEGER,
+    },
+    minimumBalance: {
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('AccountTypes'),
+};
--- server/routes/superAdmin.js
+++ server/routes/superAdmin.js
@@ -5,10 +5,18 @@ import checkAuth from '../middlewares/checkAuth';
 import checkAccessLevel from '../middlewares/checkAccessLevel';
 
 const superAdminRouter = express.Router();
-const { createStaff } = SuperAdminController;
-const { validateCreateStaff } = validationMiddleware;
+const { createStaff, createAccountType } = SuperAdminController;
+const { validateCreateStaff, validateCreateAccountType } = validationMiddleware;
 const { isSupperAdmin } = checkAccessLevel;
 
 superAdminRouter.post('/staff/create', checkAuth, isSupperAdmin, validateCreateStaff, createStaff);
+
+superAdminRouter.post(
+  '/accountType/create',
+  checkAuth,
+  isSupperAdmin,
+  validateCreateAccountType,
+  createAccountType,
+);
 
 export default superAdminRouter;
