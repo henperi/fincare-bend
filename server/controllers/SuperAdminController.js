bd6b9380da3e6ff8b3ec35e8ececa27cd5e20eaa
Henry <pienry3g@gmail.com>
2019-05-18 00:44:01 +0100

    feat(superAdmin): create and update loan types
    [Finishes #166077833]

--- server/controllers/SuperAdminController.js
+++ server/controllers/SuperAdminController.js
@@ -5,7 +5,9 @@ import response from '../helpers/responses';
 import { hashPassword } from '../helpers/passwordHelpers';
 import generateStaffId from '../helpers/generateStaffId';
 
-const { AccountType, Staff, StaffProfile } = model;
+const {
+  AccountType, LoanType, Staff, StaffProfile,
+} = model;
 const { Op } = Sequelize;
 /**
  * Controller to handle neccessary staffActions
@@ -142,16 +144,125 @@ class SuperAdminController {
             'This Account Type Name has already been used, please choose another account name',
         });
       }
-      console.log(Math.abs(minimumBalance));
+
       const updatedAccountType = await accountType.update({
         name: (name && name.trim()) || accountType.name,
-        interestRate: Math.abs(interestRate).toString() || accountType.interestRate,
-        minimumBalance: Math.abs(minimumBalance).toString() || accountType.minimumBalance,
+        interestRate:
+          (interestRate && Math.abs(interestRate).toString()) || accountType.interestRate,
+        minimumBalance:
+          (minimumBalance && Math.abs(minimumBalance).toString()) || accountType.minimumBalance,
       });
 
       const message = 'AccountType updated successfully';
 
       return response.success(res, { message, updatedAccountType });
+    } catch (errors) {
+      return response.internalError(res, { errors });
+    }
+  }
+
+  /**
+   * Method to create a new Loan Type given the loanName, interestRate, maximumAmount, & payCycle
+   * @param {object} req Request object containing the staff data
+   * @param {object} res Response object
+   * @return {object} JSON response
+   */
+  static async createLoanType(req, res) {
+    const {
+      loanName: name, interestRate, maximumAmount, payCycle,
+    } = req.body;
+
+    try {
+      const loanType = await LoanType.findOne({
+        where: {
+          [Op.or]: [
+            {
+              name: name.trim(),
+            },
+          ],
+        },
+      });
+
+      if (loanType) {
+        return response.alreadyExists(res, {
+          message: 'This Loan Type has already been created',
+        });
+      }
+
+      const newLoanType = await LoanType.create({
+        name: name.trim(),
+        interestRate,
+        maximumAmount,
+        payCycle,
+      });
+
+      const message = 'Loan Type created successfully';
+
+      return response.created(res, { message, newLoanType });
+    } catch (errors) {
+      return response.internalError(res, { errors });
+    }
+  }
+
+  /**
+   * Method to update an existing Loan Type given its loanTypeId
+   * editable fields consists of eaither of the following
+   * [accountName, interestRate, maximumAmount & payCycle]
+   * @param {object} req Request object containing the staff data
+   * @param {object} res Response object
+   * @return {object} JSON response
+   */
+  static async updateLoanType(req, res) {
+    const {
+      loanName: name, interestRate, maximumAmount, payCycle,
+    } = req.body;
+    const { loanTypeId } = req.params;
+
+    try {
+      const loanType = await LoanType.findOne({
+        where: {
+          [Op.or]: [
+            {
+              id: loanTypeId.trim(),
+            },
+          ],
+        },
+      });
+
+      if (!loanType) {
+        return response.notFound(res, {
+          message: 'This Loan Type was not found',
+        });
+      }
+
+      const isLoanNameTaken = name
+        && (await LoanType.findOne({
+          where: {
+            [Op.or]: [
+              {
+                name: name.trim(),
+              },
+            ],
+          },
+        }));
+
+      if (isLoanNameTaken && isLoanNameTaken.id !== parseInt(loanTypeId, 10)) {
+        return response.alreadyExists(res, {
+          message: 'This Loan Type Name has already been used, please choose another account name',
+        });
+      }
+
+      const updatedLoanType = await loanType.update({
+        name: (name && name.trim()) || loanType.name,
+        interestRate: (interestRate && Math.abs(interestRate).toString()) || loanType.interestRate,
+        maximumAmount:
+          (maximumAmount && Math.abs(maximumAmount).toString()) || loanType.maximumAmount,
+        payCycle: (payCycle && payCycle.trim()) || loanType.payCycle,
+      });
+
+      const message = 'Loan Type updated successfully';
+
+      return response.success(res, { message, updatedLoanType });
     } catch (errors) {
       return response.internalError(res, { errors });
     }
