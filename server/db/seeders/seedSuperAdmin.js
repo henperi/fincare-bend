3421960276f9248fcf53a19ef4d5c97819ccc3d7
Henry <pienry3g@gmail.com>
2019-05-22 15:51:16 +0100

    feat(host-app): host app on heroku
    - update package.json: add scripts to automate production build and start
    - add scripts for migration [reset, drop]
    - add server/db/config/config.js
    - delete server/db/config/config.json
    - modify .sequelizerc to use server/db/config/config.js inplace of server/db/config/config.json
    - modify server/models/index.js to use server/db/config/config.js inplace of server/db/config/config.json
    - install sequelize-cli as prod dependency
    
    [Finishes #166179924]

--- server/db/seeders/seedSuperAdmin.js
+++ server/db/seeders/seedSuperAdmin.js
@@ -1,7 +1,16 @@
 import { times } from 'lodash';
 import faker from 'faker/locale/en';
+import dotenv from 'dotenv';
+import Sequelize from 'sequelize';
+
 import model from '../../models';
 import generateAuthToken from '../../helpers/generateAuthToken';
+import generateStaffId from '../../helpers/generateStaffId';
+import { hashPassword } from '../../helpers/passwordHelpers';
+
+const { Op } = Sequelize;
+
+dotenv.config();
 
 const { Staff, StaffProfile } = model;
 faker.seed(555);
@@ -9,28 +18,47 @@ faker.seed(555);
 const seedSuperAdmin = () => {
   try {
     times(1, async () => {
-      const firstName = faker.name.firstName();
-      const lastName = faker.name.lastName();
-      const randomId = faker.random.alphaNumeric(5);
+      const {
+        SU_FIRSTNAME, SU_LASTNAME, SU_PASSWORD, SU_PHONE, SU_EMAIL,
+      } = process.env;
+
+      const staff = await Staff.findOne({
+        where: {
+          [Op.or]: [{ email: SU_EMAIL.trim() }, { level: 'SuperAdmin' }],
+        },
+      });
+
+      if (staff) {
+        const message = process.env.NODE_ENV !== 'production'
+          ? 'Either Super Admin is seeded or this email is Taken'
+          : '';
+        console.log(message);
+        return process.exit();
+      }
+
+      const hashedPassword = hashPassword(SU_PASSWORD.trim());
 
       const superAdmin = await Staff.create(
         {
-          uniqueId: randomId,
-          email: faker.internet.email(firstName, lastName),
-          password: faker.internet.password(),
+          uniqueId: generateStaffId(),
+          email: SU_EMAIL,
+          password: hashedPassword,
           level: 'SuperAdmin',
           Profile: {
-            fullname: `${firstName} ${lastName}`,
-            phone: faker.phone.phoneNumber(),
+            fullname: `${SU_FIRSTNAME} ${SU_LASTNAME}`,
+            phone: SU_PHONE,
           },
         },
         { include: [{ model: StaffProfile, as: 'Profile' }] },
       );
-      const token = generateAuthToken(superAdmin);
+
+      const token = process.env.NODE_ENV !== 'production' ? generateAuthToken(superAdmin) : '';
       console.log(token);
+      return process.exit();
     });
   } catch (error) {
     console.log(error);
+    process.exit();
   }
 };
 
